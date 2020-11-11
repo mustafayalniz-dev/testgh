d4eb67d89d34f8062460c4b4cac6e6b251a2d72b
myalniz <myyalniz@gmail.com>
2020-11-11 17:17:39 +0300

    test message

--- scripts/jira-comment.js
+++ scripts/jira-comment.js
@@ -1,33 +1,21 @@
-var child_process = require("child_process")
 const fetch = require("node-fetch")
 const promisify = require("util").promisify
 const fs = require("fs")
-const dotenv = require("dotenv")
 const readFile = promisify(fs.readFile)
 
-var commits = process.argv.slice(2)
+const JIRA_USERNAME = process.env.JIRA_USERNAME
+const JIRA_API_TOKEN = process.env.JIRA_API_TOKEN
 
 const event = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8'))
 
 const baseUrl="https://spinbikes.atlassian.net"
-const jqlSearchBaseUrl="/rest/api/2/search/?jql="
 const issueBaseUrl="/rest/api/2/issue/"
-const transitionUrl="/transitions"
-const browseUrl="/browse/"
 
 async function main() {
-    await loadJiraCredentials()
     await processCommits()
 }
 
 main()
-
-async function loadJiraCredentials() {
-    const envfile = await readFile("./.env", "utf-8")
-    const env = dotenv.parse(global.Buffer.from(envfile))
-    JIRA_USERNAME = env.JIRA_USERNAME
-    JIRA_API_TOKEN = env.JIRA_API_TOKEN
-}
 
 async function headersWithAuth(headers) {
     const auth =
@@ -52,7 +40,6 @@ async function processCommits() {
     }
     if (issueIdRegex.test(message)) {
       var issueKey=message.replace(/^\[([A-Z]+\-[0-9]+)\].+$/, "$1")
-      console.log("issueKey " + issueKey )
       await processSingleCommit(branch, id, issueKey, message, url)
     }
   }
@@ -61,25 +48,24 @@ async function processCommits() {
 
 async function processSingleCommit(branch, id, issueKey,  message, url) {
      headers = await headersWithAuth({ "Content-Type": "application/json" })
-
      var repository = url.replace(/\/commit\/.+/, "")
-
      var body = "Repository: " + repository + "\nBranch: " + branch + "\nCommit: " + id + "\nCommit message: " + message
 
      var issueComment = {
     	"body": body 
      } 
 
-     console.log(issueComment)
      jiraPushUrl = baseUrl + issueBaseUrl + issueKey + "/comment"
 
      console.log(jiraPushUrl)
+     console.log(JSON.stringify(issueComment))
+     console.log(headers)
 
-//     return await fetch(jiraPushUrl, {
-//       method: "post",
-//       body: JSON.stringify(requestBody),
-//       headers: headersToSend,
-//     })
+     return await fetch(jiraPushUrl, {
+       method: "post",
+       body: JSON.stringify(issueComment),
+       headers: headers
+     })
 
 }
 
